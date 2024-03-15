44e7c351e7223734d71442694fdad5356260c103
Nikita Tsukanov <keks9n@gmail.com>
2024-03-15 12:36:15 +0500

    Repro

---
+++ RiderCopyBug/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace RiderCopyBug;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.txt")));
+    }
+}
